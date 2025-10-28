156eca22a383aae870b52bb73c5ad4cf3e2b1ece
Ali Akbar <1aliakbar6821@gmail.com>
2025-10-28 11:21:51 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_cancunf-user
-add_lunch_combo omni_cancunf-userdebug
-add_lunch_combo omni_cancunf-eng
+add_lunch_combo twrp_cancunf-user
+add_lunch_combo twrp_cancunf-userdebug
+add_lunch_combo twrp_cancunf-eng
